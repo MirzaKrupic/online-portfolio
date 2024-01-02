db05b46eb143e1d59cc83b2d5c8542199f9e341c
Mirza Krupic <krupicmirza99@gmail.com>
2024-01-02 21:58:17 +0100

    hero div

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { motion } from "framer-motion"
+
+import { styles } from "../styles"
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] mx-auto flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-90 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#914eff]">Mirza</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I am web developer that develops <br className="block md:hidden"/> both front-end and back-end.</p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
   )
 }
 
